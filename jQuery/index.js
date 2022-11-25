0ff7e25aaa512440247db466c71931da4b70c6a7
Nishant Arora <nishant.arora03@sap.com>
2022-11-25 20:26:05 +0530

    s

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,7 @@
 
 $(document).ready(function(){
   $("h1").css("color","green");
-})
+  $("h1").addClass("backgroundBlue");
+  $("button").text("Dont click mee.");
+  $("button").html("<br/>Hey<br/>")
+});
