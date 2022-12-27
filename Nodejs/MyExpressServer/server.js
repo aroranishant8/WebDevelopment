e3561c3e52be1bf2de149be26fcd90ba4b14cf2a
Nishant Arora <nishant.arora03@sap.com>
2022-12-27 21:30:49 +0530

    about

--- Nodejs/MyExpressServer/server.js
+++ Nodejs/MyExpressServer/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/about', (req, res) => {
+  res.send('Hello,My Name is Nishant. I just begin to lean Nodejs')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
