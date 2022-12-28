5720d6d87281b6f4f4b853b2cc4fbcef825ea37d
Nishant Arora <nishant.arora03@sap.com>
2022-12-28 15:23:12 +0530

    Calculator

--- Nodejs/MyExpressServer/server.js
+++ Nodejs/MyExpressServer/server.js
@@ -10,6 +10,16 @@ app.get('/about', (req, res) => {
   res.send('Hello,My Name is Nishant. I just begin to lean Nodejs')
 })
 
+
+app.get('/contact', (req, res) => {
+  res.send('Contact me at Mysuru')
+})
+
+app.get('/nodemon', (req, res) => {
+  res.send('Nodemon will start automatically after applying changes')
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
